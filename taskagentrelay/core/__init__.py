0b224a20385901b28e0fce3006325c4ef3f39833
saginadav80 <sagi.nadav80@gmail.com>
2026-08-26 18:53:19 +0300

    feat: export TaskAgentRelay core API

--- taskagentrelay/core/__init__.py
+++ taskagentrelay/core/__init__.py
@@ -1,1 +1,22 @@
-"""TaskAgentRelay core package."""
+from .approval import ApprovalPolicy, ApprovalRequired
+from .bootstrap import build_registry, default_workspace
+from .engine import TaskEngine
+from .models import Capability, ExecutionRequest, ExecutionResult, Implementation, Task
+from .registry import Registry
+from .storage import InMemoryTaskStore, TaskStore
+
+__all__ = [
+    "ApprovalPolicy",
+    "ApprovalRequired",
+    "Capability",
+    "ExecutionRequest",
+    "ExecutionResult",
+    "Implementation",
+    "InMemoryTaskStore",
+    "Registry",
+    "Task",
+    "TaskEngine",
+    "TaskStore",
+    "build_registry",
+    "default_workspace",
+]
